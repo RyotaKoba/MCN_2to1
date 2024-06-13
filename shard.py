b374e8ed6f8c908e4cba59119270bbda0f495a04
koba <koba@mprg.cs.chubu.ac.jp>
2024-06-13 05:48:58 +0000

    fix compatibilly

--- shard.py
+++ shard.py
@@ -1,34 +1,17 @@
 from tqdm.auto import tqdm
-from PIL import Image
 import numpy as np
-import json
 import webdataset as wds
 import os
 import random
 
 dataset_root = './data/howto100M/HowTo100M_1166_videopath890000.txt'
+dir_path = "/home/kobayashi/video"
 
-file_paths = [
-    path for path in Path(dataset_root).glob('*/*')
-    if not path.is_dir() 
-        and path.name.endswith((
-            '.JPEG', '.jpeg', '.jpg',
-        ))
-]
+with open(dataset_root, "r") as file:
+    file_paths = [os.path.join(dir_path, line.strip() + ".npz") for i, line in enumerate(file) if i != 0]
 random.shuffle(file_paths)
 
-print(file_paths[:2])
-
-category_list = sorted([
-    path.name for path in Path(dataset_root).glob('*') if path.is_dir()
-    ])
-category_index = {
-    category_name: i 
-    for i, category_name in enumerate(category_list)
-    }
-
-shard_path = './data/shards_01'
-dataset_root = "/home/video/"
+shard_path = '/home/kobayashi/shard/video_01'
 
 shard_dir_path = os.pardir(shard_path)
 shard_filename = os.path.join(shard_dir_path,f"shards_video.tar")
@@ -36,24 +19,11 @@ shard_filename = os.path.join(shard_dir_path,f"shards_video.tar")
 # shard_size = int(50 * 1000**2)  # 50MB each
 
 with wds.ShardWriter(shard_filename) as sink, tqdm(file_paths) as pbar:
-
     for file_path in pbar:
-        category_name = file_path.parent.name
-        label = category_index[category_name]
-        key_str = category_name + '/' + file_path.stem
-
+        _,_,_,_,_,video_id = file_path.strip().split("/")
+        key_str = video_id.rstrip('.npz')
         sink.write({
             "__key__": key_str,
-            "jpg": np.array(Image.open(file_path)),
-            "cls": label,
+            "npz": np.load(file_path)
         })
-
-dataset_size = len(shard_filename)
-
-dataset_size_filename = str(
-    shard_dir_path / 'dataset-size.json')
-with open(dataset_size_filename, 'w') as fp:
-    json.dump({
-        "dataset size": dataset_size,
-        "n_classes": len(category_index),
-    }, fp)
+        
